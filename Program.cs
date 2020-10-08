dea90fb210f29172d0f1180fdabb4d5510480376
Mr Tony Collins <46051240+tjceltham@users.noreply.github.com>
2020-10-08 10:36:12 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Start your main subroutine here
+            // Ensure you make regular commits
+            // and sync at the end of your codinfg session to save those commits to GitHub
+        }
+    }
+}
