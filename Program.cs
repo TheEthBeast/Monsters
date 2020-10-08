4407620189645f8844fa618dc4b7c6d56f764120
BuildTools <unconfigured@null.spigotmc.org>
2020-10-08 11:03:03 +0100

    Task 1, 2, 3, 4, 5, 6

--- Program.cs
+++ Program.cs
@@ -10,9 +10,50 @@ namespace AreaTrainer
     {
         static void Main(string[] args)
         {
-            // Start your main subroutine here
-            // Ensure you make regular commits
-            // and sync at the end of your codinfg session to save those commits to GitHub
+            int a = 2;
+            while (a >= 2 && a <= 8)
+            {
+                Console.WriteLine(a);
+                a = a + 1;
+            }
+
+            int b = 10;
+            while (b >= 10 && b <= 20)
+            {
+                Console.WriteLine(b);
+                b = b + 2;
+            }
+
+            int c = 0;
+            do
+            {
+                Console.WriteLine("Enter a number:");
+                c = Convert.ToInt32(Console.ReadLine());
+            } while (c < 50);
+
+            int d = 0;
+            do
+            {
+                Console.WriteLine("Enter another number:");
+                d = Convert.ToInt32(Console.ReadLine());
+            } while (d * d < 50);
+
+            int e = 0;
+            do
+            {
+                Console.WriteLine("Enter a number:");
+                e = Convert.ToInt32(Console.ReadLine());
+            } while (e < 1 || e > 10);
+
+            int f = 0;
+            do
+            {
+                Console.WriteLine("Enter another number:");
+                f = Convert.ToInt32(Console.ReadLine());
+            } while (f < 10 && f > 5);
+
+
+            Console.ReadLine();
         }
     }
 }
